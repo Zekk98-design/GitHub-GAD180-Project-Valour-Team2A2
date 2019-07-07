a0c6878e13ef2b65ce75c5336218a8aad0863176
Chuanyang <Chuanyang10m@users.noreply.github.com>
2019-07-08 02:12:05 +1000

    2P Movement controls
    Movement controls for 2 player.
    Animations complete.

--- Assets/Scripts/Movement/CharactersMovement.cs
+++ Assets/Scripts/Movement/CharactersMovement.cs
@@ -6,12 +6,15 @@ public class CharactersMovement : MonoBehaviour
 {
     private CharacterController chaController;
     private CharacterAnimations playerAnimations;
+    
 
     public float movement_Speed = 3f;
     public float gravity = 9.8f;
     public float rotation_Speed = 0.15f;
     public float rotateDegreesPerSecond = 180f;
 
+    //temporary player selection bool
+    public bool isPlayer1;
 
     void Awake()
     {
@@ -22,7 +25,6 @@ public class CharactersMovement : MonoBehaviour
         playerAnimations = GetComponent<CharacterAnimations>();
 
     }
-
 
     // Start is called before the first frame update
     void Start()
@@ -40,51 +42,96 @@ public class CharactersMovement : MonoBehaviour
 
     void Move()
     {
-        // move foward
-        if (Input.GetAxis("Vertical") > 0)
+        
+
+        if (isPlayer1)  //if isPlayer true, then P1 control is enabled.
         {
-            Vector3 moveDirection = transform.forward;
-            moveDirection.y -= gravity * Time.deltaTime;
+            // Player1 move foward
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                Vector3 moveDirection = transform.forward;
+                moveDirection.y -= gravity * Time.deltaTime;
+                chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
+            }
+            //move backward
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                Vector3 moveDirection = -transform.forward;
+                moveDirection.y -= gravity * Time.deltaTime;
 
-            chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
+                chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
+            }
+            else chaController.Move(Vector3.zero);
         }
-
-
-        //move backward
-        else if (Input.GetAxis("Vertical") < 0)
-        {
-            Vector3 moveDirection = -transform.forward;
-            moveDirection.y -= gravity * Time.deltaTime;
-
-            chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
-        }
-
         else
-            chaController.Move(Vector3.zero);
+        {
+            // Player2 move foward
+            if (Input.GetAxis("Vertical_Player2") > 0)
+            {
+                Vector3 moveDirection = transform.forward;
+                moveDirection.y -= gravity * Time.deltaTime;
+                chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
+            }
+            //move backward
+            else if (Input.GetAxis("Vertical_Player2") < 0)
+            {
+                Vector3 moveDirection = -transform.forward;
+                moveDirection.y -= gravity * Time.deltaTime;
+
+                chaController.Move(moveDirection * movement_Speed * Time.deltaTime);
+            }
+            else chaController.Move(Vector3.zero);
+        }
 
     }//Move
 
     void Rotate()
     {
-        Vector3 rotate_Direction = Vector3.zero;
+        if (isPlayer1)
+        {   // Player1
+            Vector3 rotate_Direction = Vector3.zero;
 
-        //change the direction of RIGHT input from local to world space
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            rotate_Direction = transform.TransformDirection(Vector3.right);
+            //change the direction of RIGHT input from local to world space
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rotate_Direction = transform.TransformDirection(Vector3.right);
+            }
+
+            //change the direction of LEFT input from local to world space
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                rotate_Direction = transform.TransformDirection(Vector3.left);
+            }
+
+            //rotate the character
+            if (rotate_Direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                    Quaternion.LookRotation(rotate_Direction), rotateDegreesPerSecond * Time.deltaTime);
+            }
         }
+        else
+        {   // Player1
+            Vector3 rotate_Direction = Vector3.zero;
 
-        //change the direction of LEFT input from local to world space
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            rotate_Direction = transform.TransformDirection(Vector3.left);
-        }
+            //change the direction of RIGHT input from local to world space
+            if (Input.GetAxis("Horizontal_Player2") > 0)
+            {
+                rotate_Direction = transform.TransformDirection(Vector3.right);
+            }
 
-        //rotate the character
-        if (rotate_Direction != Vector3.zero)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,
-                Quaternion.LookRotation(rotate_Direction), rotateDegreesPerSecond * Time.deltaTime);
+            //change the direction of LEFT input from local to world space
+            if (Input.GetAxis("Horizontal_Player2") < 0)
+            {
+                rotate_Direction = transform.TransformDirection(Vector3.left);
+            }
+
+            //rotate the character
+            if (rotate_Direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                    Quaternion.LookRotation(rotate_Direction), rotateDegreesPerSecond * Time.deltaTime);
+            }
         }
 
     }// Rotate
@@ -99,9 +146,6 @@ public class CharactersMovement : MonoBehaviour
         }
         else
             playerAnimations.Walk(false);
-
-
-
     }
 
 
