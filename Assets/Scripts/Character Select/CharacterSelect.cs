73b2393c5a4558db20ade15bde356a5490896082
Connor S <8032654+mymct33@users.noreply.github.com>
2019-07-12 18:54:19 +1000

    start of character select

---
+++ Assets/Scripts/Character Select/CharacterSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    private string pTag;
+    public Button P1Warrior, P2Warrior, P1Rogue, P2Rogue, P1Mage, P2Mage;
+
+    private void Start()
+    {
+        P1Warrior.onClick.AddListener(delegate { GetTag(pTag); });
+    }
+
+    private string GetTag(string pTag)
+    {
+        pTag = gameObject.tag;
+        return pTag;
+    }
+
+    public void CharSelect(string pTag)
+    {
+        print(pTag);
+    }
+}
