6cbcd66cf457e82a242532d1b070d0816e0e9c41
Connor S <8032654+mymct33@users.noreply.github.com>
2019-07-12 20:22:26 +1000

    Character Select into player instantiation on maps

--- Assets/Scripts/Character Select/CharacterSelect.cs
+++ Assets/Scripts/Character Select/CharacterSelect.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 public class CharacterSelect : MonoBehaviour
 {
     private string pTag;
+    public string p1Char;
+    public string p2Char;
     public Button P1Warrior, P2Warrior, P1Rogue, P2Rogue, P1Mage, P2Mage;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void Start()
     {
@@ -22,5 +29,15 @@ public class CharacterSelect : MonoBehaviour
     public void CharSelect(string pTag)
     {
         print(pTag);
+
+        if (pTag == "P1Warrior" || pTag == "P1Rogue" || pTag == "P1Mage")
+        {
+            p1Char = pTag;
+        }
+
+        if (pTag == "P2Warrior" || pTag == "P2Rogue" || pTag == "P2Mage")
+        {
+            p2Char = pTag;
+        }
     }
 }
