45ee9f737611e2a13a723016def9b79b408dae93
Zack Donaldson <51683489+Zekk98-design@users.noreply.github.com>
2019-07-15 12:03:25 +1000

    Health

---
+++ Assets/Zack scripts/Stats/HealthGain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthGain : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        GameControlScript.health += 1;
+    }
+}
