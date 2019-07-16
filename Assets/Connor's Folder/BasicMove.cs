52d4630e3d7bd7ea39590ec1a44e1e06e9aff3cc
Chuanyang <Chuanyang10m@users.noreply.github.com>
2019-07-17 02:39:11 +1000

    Movement rotation issue solved.
    Movement rotation issue solved.

--- Assets/Connor's Folder/BasicMove.cs
+++ Assets/Connor's Folder/BasicMove.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BasicMove : MonoBehaviour
 {
-    public float moveSpeed = 1.0f;
+    public float moveSpeed = 6.0f;
     public float rotSpeed = 100.0f;
+   
+    Vector3 iniRotaVect = Vector3.zero;
+    Quaternion currentRotation = Quaternion.LookRotation(Vector3.zero);
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +19,67 @@ public class BasicMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        float moveVertical = Input.GetAxisRaw("Vertical") * Time.deltaTime;
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.rotation = Quaternion.LookRotation(movement);
+
+        float mHorizontal = Input.GetAxisRaw("Horizontal") ;
+        float mVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(mHorizontal, 0.0f, mVertical);
+
+        if (movement == Vector3.zero)
+        {
+            transform.rotation = currentRotation;
+            Debug.Log("There is no move Vector !!!!!!");
+
+        }
+        else
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+            transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        //transform.rotation = Quaternion.FromToRotation(transform.position, movement);
+        //transform.rotation = Quaternion.Euler(new Vector3(0, mHorizontal*90f+mVertical*90f
+        //    , 0));
 
 
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
-    
+        // when rotate to right, face right
+        if (transform.eulerAngles.y == 90.0f)
+        {
+            currentRotation = Quaternion.LookRotation(Vector3.right);
+
+            Debug.Log("give me Right currentRotation.y: " + transform.eulerAngles.y);
+        }
+
+        
+        // when rotate to left, face left
+        if (transform.eulerAngles.y == 270f)
+        {
+            currentRotation = Quaternion.LookRotation(Vector3.left);
+            Debug.Log("give me Left currentRotation.y: " + transform.eulerAngles.y);
+        }
+       
+        // when rotate to up, face it
+        if (transform.eulerAngles.y == 0.0f)
+        {
+            currentRotation = Quaternion.LookRotation(Vector3.forward);
+
+            Debug.Log("give me Up currentRotation.y: " + transform.eulerAngles.y);
+        }
+        
+        // when rotate to down, face it
+        if (transform.eulerAngles.y == 180f)
+        {
+            currentRotation = Quaternion.LookRotation(Vector3.back);
+            Debug.Log("give me Down currentRotation.y: " + transform.eulerAngles.y);
+        }
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ Assets/Scripts/Movement/MoveTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    public float moveSpeed = 7.0f;
+    public float rotateSpeed = 10;
+    private Rigidbody rb;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.constraints = RigidbodyConstraints.FreezeRotationY;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        // Read inputs
+        float mHorizontal = Input.GetAxis("Horizontal");
+        float mVertical = Input.GetAxis("Vertical");
+
+        //Vector3 moveForward = mVertical*  Vector3.forward* moveSpeed;
+        //Vector3 turnDirection = mHorizontal * Vector3.right * moveSpeed ;
+
+        if (mHorizontal > 0)
+        {
+            //Rotate to the right
+            rb.rotation = Quaternion.LookRotation(transform.right) ;
+            //Move always to the forward vector
+            rb.MovePosition(transform.position + transform.forward * moveSpeed* Time.deltaTime) ;
+        }
+        else if (mHorizontal < 0)
+        {
+            //Rotate to the left
+            transform.rotation = Quaternion.LookRotation(-transform.right * Time.deltaTime);
+            //Move always to the forward vector
+            rb.MovePosition(transform.position + transform.forward * moveSpeed* Time.deltaTime);
+        }
+
+        if (mVertical > 0)
+        {
+            ////Rotate to the right
+            //transform.rotation = Quaternion.LookRotation(transform.right);
+            //Move always to the forward vector
+            rb.MovePosition(transform.position + transform.forward * moveSpeed* Time.deltaTime);
+        }
+        else if(mVertical < 0)
+        {
+            ////Rotate to the left
+            //transform.rotation = Quaternion.LookRotation(-transform.right);
+            //Move always to the forward vector
+            rb.MovePosition(transform.position + transform.forward*-1f * moveSpeed* Time.deltaTime);
+        }
+
+
+
+
+        //Vector3 movement = moveForward + turnDirection;
+        //rb.MovePosition(rb.position + movement);
+    }
+    
+
+
+}
+
+
