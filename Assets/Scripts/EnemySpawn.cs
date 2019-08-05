43a146e251b8cf85439eb904082a58610dc2098d
Connor S <8032654+mymct33@users.noreply.github.com>
2019-08-05 18:51:22 +1000

    Enemy spawn now has upper limit
    ^ but no longer scales as the game progresses

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     public int maxSpawn = 3;
-    public int iniSpawn = 1;
+    public int iniSpawn = 0;
     public int bbegSpawn = 0;
     public float spawnRadius = 3.0f;
     private List<Vector3> spawnPoints = new List<Vector3>();
@@ -14,15 +14,16 @@ public class EnemySpawn : MonoBehaviour
     public Vector3 spawn;
     public Rounds rounds;
     private float spawnTimer = 1.0f;
+    [SerializeField] private int upperLimit = 10;
 
     void Update()
     {
         spawnTimer = spawnTimer - Time.deltaTime;
+        maxSpawn = upperLimit - GameObject.FindGameObjectsWithTag("Enemy").Length;
         if (spawnTimer <= 0 & rounds.timer > 0)
         {
             GetLocation();
             spawnTimer = 10.0f;
-            maxSpawn = maxSpawn + iniSpawn;
         }
     }
 
@@ -36,7 +37,7 @@ public class EnemySpawn : MonoBehaviour
     //Generating locations and instantiating model
     public Vector3 GetLocation()
     {
-        while (iniSpawn <= maxSpawn)
+        while (iniSpawn <= maxSpawn && maxSpawn <= upperLimit)
         {
             spawn = new Vector3(Random.Range(-spawnRadius, spawnRadius), 1, Random.Range(-spawnRadius, spawnRadius));
 
@@ -51,6 +52,7 @@ public class EnemySpawn : MonoBehaviour
                 Instantiate(charModel, transform.position + spawn, transform.rotation);
             }
         }
+        iniSpawn = 0;
         return spawn;
     }
 }
