aa6405ee92cb82c56a667579278cd606c64d2ef7
Connor S <mymct33@gmail.com>
2019-08-12 11:29:32 +1000

    UI and Health are on speaking terms
    it works

---
+++ Assets/Connor's Folder/UIHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealth : MonoBehaviour
+{
+    public GameObject bar1, bar2, bar3, bar4, bar5, bar6, bar7, bar8, bar9, gameOver;
+    public GameObject p2Bar1, p2Bar2, p2Bar3, p2Bar4, p2Bar5, p2Bar6, p2Bar7, p2Bar8, p2Bar9, p2GameOver;
+    public GameObject shield1, shield2, shield3, shield4, shield5, shield6, shield7, shield8, shield9, shieldGone;
+    public GameObject p2Shield1, p2Shield2, p2Shield3, p2Shield4, p2Shield5, p2Shield6, p2Shield7, p2Shield8, p2Shield9, p2ShieldGone;
+}
