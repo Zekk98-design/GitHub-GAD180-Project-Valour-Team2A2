4be039116f4d3809f760b966f2595f839ab56856
Chuanyang10m <Chuanyang10m@users.noreply.github.com>
2019-08-19 12:49:40 +1000

    Fixed Warrior attack, Leaderboard UI resize
    - and also added Back button in Leaderboard.

--- Assets/Scripts/Attack/MeleeAttack.cs
+++ Assets/Scripts/Attack/MeleeAttack.cs
@@ -1,30 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// attach this to Sword only
+/// </summary>
 public class MeleeAttack : MonoBehaviour
 {
     public Leaderboard lBoard;
     public PlayerStats playerStats;
     public bool isWeapon;
-
+    public bool isAttacked;
     
 
     private void Start()
     {
         GameObject EGO = GameObject.Find("Game Manager");
         lBoard = EGO.GetComponent<Leaderboard>();
+        isAttacked = false;
     }
 
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            isAttacked = true;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            isAttacked = true;
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            isAttacked = true;
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            isAttacked = true;
+        }
+    }
     private void OnTriggerEnter(Collider other)                                      // Check collision
     {
+        
         if (other.gameObject.tag == "Enemy")                                            // Compare collision with tag
         {
             EnemyStats enemyStats = other.gameObject.GetComponent<EnemyStats>();             // Get stats on collided enemy
  
-            if (isWeapon == true && enemyStats.defence > 0 )
+            if (isWeapon == true && enemyStats.defence > 0 && isAttacked ==true)
             {
                 enemyStats.defence = enemyStats.defence - playerStats.damage ;
+
+                isAttacked = false;
                                 
                 if (gameObject.CompareTag("Player1"))
                 {
@@ -36,10 +63,12 @@ public class MeleeAttack : MonoBehaviour
                 }
             }
 
-            if (isWeapon == true && enemyStats.defence <= 0)
+            if (isWeapon == true && enemyStats.defence <= 0 && isAttacked ==true)
             {
                 enemyStats.health = enemyStats.health - playerStats.damage;                 // if weapon, deal damage
-                
+
+                isAttacked = false;
+
                 if (gameObject.CompareTag("Player1"))
                 {
                     lBoard.p1Dmg = lBoard.p1Dmg + playerStats.damage;
